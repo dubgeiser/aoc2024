a067a1784c3e7180209bbd50018ad2a55cdd075d
Per Juchtmans <per@dubgeiser.net>
2024-12-04 12:27:22 +0100

    Day 4, part 2

--- 04/main.go
+++ 04/main.go
@@ -53,7 +53,42 @@ func (s *Solution) Part1() any {
 }
 
 func (s *Solution) Part2() any {
-	return "NOT IMPLEMENTED"
+	total := 0
+	R := len(s.G)
+	C := len(s.G[0])
+	for r := 0; r < R; r++ {
+		for c := 0; c < C; c++ {
+			// sentinel: if middle is not A, disregard
+			if r+1 < R && c+1 < C && s.G[r+1][c+1] != 'A' {
+				continue
+			}
+			// M M
+			//  A
+			// S S
+			if r+2 < R && c+2 < C && s.G[r][c] == 'M' && s.G[r][c+2] == 'M' && s.G[r+2][c] == 'S' && s.G[r+2][c+2] == 'S' {
+				total++
+			}
+			// M S
+			//  A
+			// M S
+			if r+2 < R && c+2 < C && s.G[r][c] == 'M' && s.G[r][c+2] == 'S' && s.G[r+2][c] == 'M' && s.G[r+2][c+2] == 'S' {
+				total++
+			}
+			// S M
+			//  A
+			// S M
+			if r+2 < R && c+2 < C && s.G[r][c] == 'S' && s.G[r][c+2] == 'M' && s.G[r+2][c] == 'S' && s.G[r+2][c+2] == 'M' {
+				total++
+			}
+			// S S
+			//  A
+			// M M
+			if r+2 < R && c+2 < C && s.G[r][c] == 'S' && s.G[r][c+2] == 'S' && s.G[r+2][c] == 'M' && s.G[r+2][c+2] == 'M' {
+				total++
+			}
+		}
+	}
+	return total
 }
 
 func main() {
