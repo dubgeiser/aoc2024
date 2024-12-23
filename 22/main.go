d3e901e4b84407d06fbc027e026eed0be28cbd27
Per Juchtmans <per@dubgeiser.net>
2024-12-23 21:05:18 +0100

    Day 22, part 2

--- 22/main.go
+++ 22/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"os"
 	"strconv"
 	"strings"
 )
+
+func newSecret(n int) int {
+	n = (n ^ (n * 64)) % 16777216
+	n = (n ^ (n / 32)) % 16777216
+	n = (n ^ (n * 2048)) % 16777216
+	return n
+}
 
 func main() {
 	fmt.Println()
@@ -14,15 +22,43 @@ func main() {
 		panic("Cannot read input!")
 	}
 	lines := strings.Split(strings.TrimSpace(string(raw)), "\n")
-	count := 0
+	part1 := 0
+	seq2totalScore := map[[4]int]int{}
 	for _, line := range lines {
 		n, _ := strconv.Atoi(line)
+		prices := []int{}
 		for i := 0; i < 2000; i++ {
-			n = (n ^ (n * 64)) % 16777216
-			n = (n ^ (n / 32)) % 16777216
-			n = (n ^ (n * 2048)) % 16777216
+			n = newSecret(n)
+			prices = append(prices, n%10)
 		}
-		count += n
+		part1 += n
+		changes := []int{}
+		for i := 0; i < len(prices)-1; i++ {
+			changes = append(changes, prices[i+1]-prices[i])
+		}
+		seq2score := map[[4]int]int{}
+		for i := 0; i < len(changes)-3; i++ {
+			seq := [4]int{changes[i], changes[i+1], changes[i+2], changes[i+3]}
+			_, ok := seq2score[seq]
+			if !ok {
+				seq2score[seq] = prices[i+4]
+			}
+		}
+		for seq, score := range seq2score {
+			_, ok := seq2totalScore[seq]
+			if ok {
+				seq2totalScore[seq] += score
+			} else {
+				seq2totalScore[seq] = score
+			}
+		}
 	}
-	fmt.Println(count)
+	fmt.Println(part1)
+	max := 0
+	for s := range maps.Values(seq2totalScore) {
+		if s > max {
+			max = s
+		}
+	}
+	fmt.Println(max)
 }
