93a1b69b45e407c0862f28ba07219b46cd0836a4
Per Juchtmans <per@dubgeiser.net>
2025-01-04 00:01:08 +0100

    Day 23, part 2

--- 23/main.go
+++ 23/main.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 )
 
-
-
 func main() {
 	// network maps computer to all the computers it is connected to.
 	// We store each link, bidirectional
@@ -54,4 +52,54 @@ func main() {
 		}
 	}
 	fmt.Println(sets.Len())
+
+	// --[ Part 2 ]--
+	// Collect all the LANs and print out the longest.
+	lans := set.New[string]()
+	longest := ""
+	for c := range network {
+		mklans(c, []string{c}, lans, network)
+	}
+	for l := range lans.Values() {
+		if len(l) > len(longest) {
+			longest = l
+		}
+	}
+	fmt.Println(longest)
+}
+
+func mklans(computer string, lanCurr []string, lans *set.Set[string], network map[string]*set.Set[string]) {
+	// We need a comparable to store in sets.
+	// Since we need to represent the longest LAN as a comma separated, ordered,
+	// list of computers, we already pre-format.
+	slices.Sort(lanCurr)
+	currs := strings.Join(lanCurr, ",")
+
+	// If we processed the current LAN already, this means that the current LAN
+	// is the largest one we could build for the given computer, ie.
+	// `allConnected` was false in the previous call and nothing has been added
+	// to `lanCurr`.
+	if lans.Contains(currs) {
+		return
+	}
+	lans.Add(currs)
+
+	// Recursive DFS the network to find the LAN (ie. all the computers that
+	// have connections to each other).
+	for cc := range network[computer].Values() {
+		if slices.Contains(lanCurr, cc) {
+			continue
+		}
+		allConnected := true
+		for _, sc := range lanCurr {
+			if !network[sc].Contains(cc) {
+				allConnected = false
+				break
+			}
+		}
+		if allConnected {
+			lanCurr = append(lanCurr, cc)
+		}
+		mklans(cc, lanCurr, lans, network)
+	}
 }
